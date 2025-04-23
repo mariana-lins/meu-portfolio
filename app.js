e0028c7b3404b5ad97761e6339c2fb8a37685108
Mariana Lins <84293588+mariana-lins@users.noreply.github.com>
2025-04-23 00:35:47 +0000

    portifolio

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Meu Portfólio' });
+});
+
+app.get('/projetos', (req, res) => {
+  res.render('projetos', { title: 'Projetos' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
