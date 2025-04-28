45a7504574dd55a4abcf9664d9923e5526aed28e
Mariana Lins <mmarianalinss@gmail.com>
2025-04-28 10:00:44 -0300

    final apresentado encerrar qnd mudar variavel reiniciar

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Meu Portfólio', nome: 'Mariana Cavalcante Lins' });
+  res.render('index', { title: 'Meu Portfólio', nome: 'Mariana Lins' });
 });
 
 app.get('/projetos', (req, res) => {
