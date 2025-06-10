8a634198122de0050f3bedb362f0796293a7aa19
Mariana Lins <84293588+mariana-lins@users.noreply.github.com>
2025-06-10 02:04:16 +0000

    cadastrar

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.urlencoded({ extended: true })); // Adicione esta linha
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,8 +12,16 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Meu Portfólio', nome: 'Mariana Cavalcante Lins' });
 });
 
+const projetos = [];
+
 app.get('/projetos', (req, res) => {
-  res.render('projetos', { title: 'Projetos' });
+  res.render('projetos', { title: 'Projetos', projetos });
+});
+
+app.post('/projetos', (req, res) => {
+  const { titulo, descricao, tecnologias, link } = req.body;
+  projetos.push({ titulo, descricao, tecnologias, link });
+  res.redirect('/projetos');
 });
 
 const port = 3000;
